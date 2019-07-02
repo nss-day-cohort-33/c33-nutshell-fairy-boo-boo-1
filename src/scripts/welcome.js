dbe18982e21c5970920f3fde3daa29aafcfb329e
Karla Gallegos <kgalleg618@gmail.com>
2019-07-02 15:25:39 -0500

    Welcome page registration progress

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,7 +1,14 @@
 import { API } from "./api.js"
 
 const welcomeTitle = document.querySelector("#container")
-welcomeTitle.innerHTML = "<h2>Welcome to Nutshell</h2>"
+welcomeTitle.innerHTML = "<h1>Welcome to Nutshell</h1>"
+const loginHeader = document.createElement("h4")
+loginHeader.textContent = "If you are a returning user, login here:"
+loginHeader.setAttribute("id", "loginHeaderId")
+const newUserHeader = document.createElement("a")  //anchor element for registration link
+newUserHeader.textContent = "If you are a new user, click here to register"
+newUserHeader.setAttribute("href", "#")
+newUserHeader.setAttribute("id", "registerUserLinkId")
 
 //Creating Elements for Registration Login
 const registrationParentDiv = document.createElement("div")
@@ -17,9 +24,12 @@ userRegistration.setAttribute("id", "registrationUserId")
 registerButton.setAttribute("id", "registerButtonId")
 
 //Attaching Elements to the DOM
+registrationParentDiv.appendChild(loginHeader)
 registrationParentDiv.appendChild(passwordRegistration)
 registrationParentDiv.appendChild(userRegistration)
 registrationParentDiv.appendChild(registerButton)
+registrationParentDiv.appendChild(newUserHeader)
+
 
 //Putting it in the DOM
 const outputLocation = document.querySelector("#container")
@@ -44,7 +54,43 @@ registerButton.addEventListener("click", ()=> {
     console.log(valueRegistrationUser)
 })
 
+newUserHeader.addEventListener("click", ()=> {
+//TODO: console.log("link clicked")
+document.querySelector("#registerButtonId").style.display = "none"
+document.querySelector("#loginHeaderId").textContent = "Please Register Below"
+document.querySelector("#registerUserLinkId").style.display = "none"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //TODO: fix regiter word in variable names
+
+//TODO:
+//1.  Under Welcome to Nutshell, place a heading element (give it an id to grab it later) in a line that says--"If you're a returning user, login in here:"
+// 2.  Under the userid and password input fields and login button, place an another heading element (give it an id to grab it later) and an anchor element within it  that says--If you're a new user, <click here to register>.  When they click this, change the messages, and display a register button.
+// 3.  If they click the login button, check to see if they are an existing user and if NOT, ask them if they want to register.  If they want to register, sent them to the registration screen (has a register button on it).  If they don't want to register, (stretch goal?-->tell them thanks for visiting), and return them to the login screen.
+// 4.  If they click the register choice, then have them enter the information to register (and change the button to the register button).   After entering the data, if they exist in the system, put a message "You already have an account with us.  Please login."  And send them back to the login screen (has a login button).
+
+
+
+
+
+
+
 
 
 
