370b9ffa0112ee8ede6e3885c0a125446b15c13c
Jake Scott <bemybanker@gmail.com>
2019-07-02 18:43:15 -0500

    I hopethis works

---
+++ src/scripts/articles.js
@@ -0,0 +1,17 @@
+
+let time = console.log("is this working")
+
+let limmy = console.log("I wanna fuckn party")
+
+
+
+
+
+
+
+
+
+
+
+
+export{time, limmy}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,4 @@
+import {time, limmy} from "./articles.js"
+
+
 console.log("Your Webpack application is set up and ready to go. Please start writing code.")
